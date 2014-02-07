b631b53178d22f7204fbbae57e4ed8ff350e03e4
ThomasHoet <hoet.thomas@gmail.com>
2014-02-07 16:39:03 +0100

    HelloWorld3

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@ import lejos.nxt.Button;
 
 public class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello World3");
     Button.waitForAnyPress();
   }
 }
