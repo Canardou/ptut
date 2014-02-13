1519d6c6a2f2a01577d9424505d5d24b42f8b1b8
ThomasHoet <hoet.thomas@gmail.com>
2014-02-13 18:02:11 +0100

    Firmware 1
    aussi nommé "i have no idea what im doin'"

---
+++ Firmware 1/src/Compass.java
@@ -0,0 +1,51 @@
+import lejos.nxt.I2CPort;
+import lejos.nxt.addon.CompassHTSensor;
+
+public class Compass extends CompassHTSensor {	
+	
+	/****************/
+	/** CONSTANTES **/
+	/****************/
+	public static final int TAB_NBDATA = 3;
+	
+	/***************/
+	/** ATTRIBUTS **/
+	/***************/
+	private double data[];
+	private int idxData;
+	protected double moyData;
+	protected double desAngle;
+	
+	
+	 /******************/
+	 /** CONSTRUCTEUR **/
+	 /******************/
+	 public Compass (I2CPort port) {
+		 super(port);
+		 this.data = new double[TAB_NBDATA];
+		 this.idxData = 0 ;
+		 this.moyData = 0 ;
+		 this.desAngle = 0 ;
+	 }
+	 
+	 /**************/
+	 /** METHODES **/
+	 /**************/
+	 
+	 //Retourne la moyenne du tableau de valeurs
+	 public double moyenne() {
+		 int i;
+		 double sum = 0;
+		 for(i=0;i<TAB_NBDATA;i++){
+			 sum+=this.data[i];
+		 }
+		 return (sum/TAB_NBDATA);
+	 }
+	 
+	 //Acquisitionne une valeur de distance et la place dans le tableau
+	 public void acquisition() {
+		 this.data[this.idxData] = this.getDegreesCartesian();
+		 this.idxData = (this.idxData+1)%TAB_NBDATA;
+	 } 
+	
+}
