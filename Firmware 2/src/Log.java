148ed15fc8e6a4e9c6ba99f5a1cac9e07f62071e
Canardou <hit_@hotmail.fr>
2014-02-18 18:59:55 +0100

    Logs

--- Firmware 2/src/Log.java
+++ Firmware 2/src/Log.java
@@ -1,5 +1,5 @@
 import lejos.nxt.Button;
-
+import lejos.nxt.LCD;
 import java.util.Stack;
 
 public class Log {
@@ -22,41 +22,58 @@ public class Log {
 		 this.size=MaxSize;
 	 }
 	 
+	 public Log(){
+		 this.pile=new Stack<String>();
+		 this.size=-1;
+	 }
+	 
 	 /**************/
 	 /** METHODES **/
 	 /**************/
 	 
 	 public void addLog(String message){
-		 if(this.pile.empty())
-			this.pile.push(message);
-		 else if(this.pile.size()<this.size)
+		 if(this.pile.size()<this.size || this.size==-1)
 		 	this.pile.push(message);
 	 }
 	 
 	 public void showAllLogs(){
 		 String temp;
-		 if(this.pile.size()>0){
+		 int i, num=this.pile.size();
+		 if(num>0){
 	     temp=this.pile.pop();
 	     this.showAllLogs();
-		 System.out.println(temp);
+	     LCD.drawString("Log "+num+" :",0,0);
+	     for(i=0;i<=Math.min(temp.length()/16,6);i++){
+	     	LCD.drawString(temp.substring(i*16, Math.min(temp.length(),(i+1)*16)),0,i+1);
+		 }
 		 Button.waitForAnyPress();
+		 LCD.clear();
 		 }
 	 }
 	 
 	 public void showDescLogs(){
-		 int i;
+		 int i, j, num=this.pile.size();
 		 String temp;
-		 for(i=0;i<this.pile.size();i++){
-		 temp=this.pile.pop();
-		 System.out.println(temp);
-		 Button.waitForAnyPress();
+		 for(i=0;i<num;i++){
+			 LCD.drawString("Log "+this.pile.size()+" :",0,0);
+			 temp=this.pile.pop();
+			 for(j=0;j<=Math.min(temp.length()/16,6);j++){
+				 LCD.drawString(temp.substring(j*16, Math.min(temp.length(),(j+1)*16)),0,j+1);
+			 }
+			 Button.waitForAnyPress();
+			 LCD.clear();
 		 }
 	 }
 	 
 	 public void showLastLog(){
 		 String temp;
+		 int i;
 		 temp=this.pile.peek();
-		 System.out.println(temp);
+		 LCD.drawString("Log "+this.pile.size()+" :",0,0);
+		 for(i=0;i<=Math.min(temp.length()/16,6);i++){
+			 LCD.drawString(temp.substring(i*16, Math.min(temp.length(),(i+1)*16)),0,i+1);
+		 }
 		 Button.waitForAnyPress();
+		 LCD.clear();
 	 }
 }
