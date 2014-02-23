dc8476ebef20c35c3156b57fb679045ce1e17e0f
Canardou <hit_@hotmail.fr>
2014-02-23 21:31:12 +0100

    Package Labyrinth
    Comprends Carte, Case, Chemin, ListeCase. Auncun test effectués.

---
+++ RobertoV-PC/src/labyrinth/Carte.java
@@ -0,0 +1,185 @@
+package labyrinth;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 
+ * @author canard
+ * 
+ */
+
+public class Carte {
+	/*
+	 * Attributs
+	 */
+	
+	private int heigth;
+	private int width;
+	private Case[][] map;
+	private Case exit;
+	
+	/*
+	 * Constructeurs
+	 */
+	
+	Carte(int width, int heigth){
+		this.heigth = heigth;
+		this.width = width;
+		this.map = new Case[width][heigth];
+		for(int i=0;i<this.heigth;i++){
+			for(int j=0;j<this.width;j++){
+				this.map[i][j]=new Case(i,j);
+			}
+		}
+	}
+	
+	Carte(int side){
+		this(side,side);
+	}
+	
+	/*
+	 * Méthodes
+	 */
+
+	/**
+	 * 
+	 * @method closeUp
+	 * @method closeDown
+	 * @method closeLeft
+	 * @method closeRight
+	 * @desc Respectivement déclarer impraticable un chemin depuis la case
+	 * 
+	 */
+	
+	public void boundUp(int x, int y){
+		if(y>0){
+			this.map[x][y].bound(this.map[x][y-1]);
+		}
+		else{
+			this.boundExit(x,y);
+		}
+			
+	}
+	
+	public void boundDown(int x, int y){
+		if(y<this.heigth-1){
+			this.map[x][y].bound(this.map[x][y+1]);
+		}
+		else{
+			this.boundExit(x,y);
+		}
+	}
+	
+	public void boundLeft(int x, int y){
+		if(x>0){
+			this.map[x][y].bound(this.map[x-1][y]);
+		}
+		else{
+			this.boundExit(x,y);
+		}
+	}
+
+	public void boundRight(int x, int y){
+		if(x<this.width-1){
+			this.map[x][y].bound(this.map[x+1][y]);
+		}
+		else{
+			this.boundExit(x,y);
+		}
+	}
+	
+	public void boundExit(int x, int y){
+		this.exit=new Case(x,y);
+		this.map[x][y].bound(this.exit);
+	}
+	
+	/**
+	 * 
+	 * @method close
+	 * @param {int} position de départ x
+	 * @param {int} position de départ y
+	 * @param {int} position d'arrivee x
+	 * @param {int} position d'arrivee y
+	 * @desc déclare un chemin impraticable entre deux cases adjacentes (ou bord)
+	 * 
+	 */
+	
+	public void bound(int dx, int dy, int ax, int ay){
+		if(dx-ax==-1 && dy-ay==0)
+			this.boundRight(dx,dy);
+		if(dx-ax==1 && dy-ay==0)
+			this.boundLeft(dx,dy);
+		if(dx-ax==0 && dy-ay==-1)
+			this.boundDown(dx,dy);
+		if(dx-ax==0 && dy-ay==1)
+			this.boundUp(dx,dy);
+	}
+	
+	/**
+	 * 
+	 * @method visite
+	 * @param {int} x
+	 * @param {int} y
+	 * @desc déclare une case déjà visitée
+	 * 
+	 */	
+	public void visite(int x, int y){
+		this.map[x][y].visite();
+	}
+
+	/**
+	 * 
+	 * @method marqueur
+	 * @param {int} x
+	 * @param {int} y
+	 * @param {bool}
+	 * @desc mettre true si le marqueur est sur la case, false sinon
+	 * 
+	 */
+	public void marqueur(int x, int y, boolean marque){
+		this.map[x][y].marqueur(marque);
+	}
+	
+	/**
+	 * 
+	 * @method createPath
+	 * @param {int} position de départ x
+	 * @param {int} position de départ y
+	 * @param {int} position d'arrivee x
+	 * @param {int} position d'arrivee y
+	 * @desc retourne un chemin entre deux emplacements
+	 * 
+	 */
+	
+	private int distance(int a, int b, int c, int d){
+		return a-c+b-d;
+	}
+	
+	public Chemin createPath(int dx, int dy, int ax, int ay){
+		Chemin path = new Chemin();
+		ArrayList<ListeCase> recherche=new ArrayList<ListeCase>();
+		ArrayList<ListeCase> closed=new ArrayList<ListeCase>();
+		ArrayList<Case> check=new ArrayList<Case>();
+		recherche.add(new ListeCase(this.map[dx][dy],0,null));
+		recherche.get(0).setDistance(this.distance(dx, dy, ax, ay));
+		check.add(this.map[dx][dy]);
+		while(recherche.size()>0 && recherche.get(0).current()!=this.map[ax][ay]){
+			for(int k=0;k<recherche.get(0).current().boundSize();k++){
+				if(!check.contains(this.map[dx][dy].get(k))){
+					recherche.add(new ListeCase(this.map[dx][dy].get(k),recherche.get(0).getCout()+1,recherche.get(0)));
+					recherche.get(0).setDistance(this.distance(dx, dy, ax, ay));
+					check.add(this.map[dx][dy].get(k));
+				}
+			}
+			closed.add(recherche.get(0));
+			recherche.remove(0);
+			Collections.sort(recherche);
+		}
+		ListeCase temp = recherche.get(0);
+		while(temp.current()!=this.map[dx][dy]){
+			path.add(temp.current());
+			temp=temp.previous();
+		}
+		return path;
+	}
+}
---
+++ RobertoV-PC/src/labyrinth/Case.java
@@ -0,0 +1,55 @@
+package labyrinth;
+import java.util.ArrayList;
+
+public class Case {
+	/*
+	 * Attributs
+	 */
+	
+	private Object robot;
+	private int marque;
+	private boolean decouverte;
+	private ArrayList<Case> voisines;
+	private int x;
+	private int y;
+	
+	/*
+	 * Constructeurs
+	 */
+	
+	Case(int x, int y){
+		this.marque=-1;
+		this.robot=null;
+		this.decouverte=false;
+		this.x=x;
+		this.y=y;
+		this.voisines=new ArrayList<Case>();
+	}
+	
+	/*
+	 * Méthodes
+	 */
+	
+	public void bound(Case voisine){
+		this.voisines.add(voisine);
+	}
+	
+	public int boundSize(){
+		return this.voisines.size();
+	}
+	
+	public void visite(){
+		this.decouverte=true;
+	}
+	
+	public void marqueur(boolean marque){
+		if(marque=true)
+			this.marque=1;
+		else
+			this.marque=0;
+	}
+	
+	public Case get(int index){
+		return this.voisines.get(index);
+	}
+}
